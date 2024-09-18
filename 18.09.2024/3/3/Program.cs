1e62a2952d7402f7bb239c1540aeb12b0331e32a
appstore54 <145669973+appstore54@users.noreply.github.com>
2024-09-18 13:59:27 +0200

    Add files via upload

---
+++ 18.09.2024/3/3/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        //1
+        double pi = 3.14;
+        //2
+        String Imie ="Jakub Moćko";
+        //3
+        int x = 10;
+        float y = (int)x;
+        //4
+        double a = 10.78493;
+        int b = (int) a;
+        Console.WriteLine(b);
+        //5
+        Console.WriteLine("Podaj tekst do napisu");
+        String text="";
+        Console.ReadLine();
+        Console.WriteLine(text);
+    }
+}
